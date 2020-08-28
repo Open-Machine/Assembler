ccd694c8f9628dd51a6a6a9abc267455e9162d63
Luca Dillenburg <luca.assumpcao.dillenburg@gmail.com>
2020-08-28 04:16:24 -0300

    :heavy_check_mark: TEST: Prevent instructions to be added on explanation map or config map only
    Signed-off-by: Luca Dillenburg <luca.assumpcao.dillenburg@gmail.com>

--- core/instruction/instructionsconfig_test.go
+++ core/instruction/instructionsconfig_test.go
@@ -1,9 +1,36 @@
 package instruction
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/open-machine/assembler/config/instructionsexplanation"
+)
 
 func TestAmountInstructions(t *testing.T) {
 	if len(instructions) != 12 {
-		t.Errorf("Expected 12 instructions explanations! ALSO CHANGE 'INSTRUCTION_TEST.GO'")
+		t.Errorf("Expected 12 instructions explanations! Change 'intructions_test.go' and 'config/instructionsexplanation/*'")
 	}
+}
+
+func TestSameKeysAsConfig(t *testing.T) {
+	instructionsExplanation := instructionsexplanation.GetInstructionsExplanation()
+
+	if len(instructions) != len(instructionsExplanation) {
+		t.Errorf("Amnt of explanations on the config and explanations are different!")
+	}
+
+	for explanationKey, _ := range instructionsExplanation {
+		_, exists := instructions[explanationKey]
+		if !exists {
+			t.Errorf("Instruction '%s' has explanation but no configuration!", explanationKey)
+		}
+	}
+
+	for configKey, _ := range instructions {
+		_, exists := instructionsExplanation[configKey]
+		if !exists {
+			t.Errorf("Instruction '%s' has configuration but no explanation!", configKey)
+		}
+	}
+
 }
